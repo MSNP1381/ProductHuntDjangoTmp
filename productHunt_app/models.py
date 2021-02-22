8a557977f787e0065895c6478d6ac4dd27d03ab3
mohamad Nemat Poor <mohamadnematpoor@gmail.com>
2021-02-22 22:53:38 +0330

    create product complete

--- productHunt_app/models.py
+++ productHunt_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    pub_date = models.DateTimeField(auto_now=True)
+    vote_tot = models.PositiveSmallIntegerField(default=1)
+    image = models.ImageField(upload_to='media/')
+    icon = models.ImageField(upload_to='media/')
+    body = models.TextField(blank=True)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:75]
--- productHunt_app/views.py
+++ productHunt_app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+@login_required
+def create_product(request):
+    if request.method == 'POST':
+        prod = Product()
+        prod.title = request.POST['title']
+        prod.body = request.POST['body']
+        prod.url = request.POST['url']
+        prod.image = request.FILES['image']
+        prod.icon = request.FILES['icon']
+        prod.hunter=request.user
+        prod.save()
+        return redirect('home')
+        # return render(request,'home.html',{'message':"""dear hunter
+        # your Product is saved successfully &#128525"""})
+    else:
+        return render(request, 'producthunt/createProduct.html')
+
