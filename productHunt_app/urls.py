3edeee48d54211099be68f5d7e8affd43cb2149c
mohamad Nemat Poor <mohamadnematpoor@gmail.com>
2021-02-23 13:36:04 +0330

    product details complet

--- productHunt_app/urls.py
+++ productHunt_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_product, name='create'),
+    path('<int:product_id>/', views.product_details, name='details'),
     # path('create',views.create_product),
 
 ]
--- productHunt_app/views.py
+++ productHunt_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 
@@ -17,11 +17,15 @@ def create_product(request):
         prod.url = request.POST['url']
         prod.image = request.FILES['image']
         prod.icon = request.FILES['icon']
-        prod.hunter=request.user
+        prod.hunter = request.user
         prod.save()
-        return redirect('home')
+        return redirect('/product/' + str(prod.id))
         # return render(request,'home.html',{'message':"""dear hunter
         # your Product is saved successfully &#128525"""})
     else:
         return render(request, 'producthunt/createProduct.html')
 
+
+def product_details(request, product_id):
+    obj = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product-details.html', {'product': obj})
